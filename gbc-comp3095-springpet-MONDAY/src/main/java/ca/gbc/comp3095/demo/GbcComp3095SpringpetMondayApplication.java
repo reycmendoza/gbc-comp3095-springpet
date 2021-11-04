90907e5df0b856c74d53f4af3e74352b9e53819c
reycmendoza <iamreycmendoza@gmail.com>
2021-11-04 11:29:12 -0400

    First Commit

---
+++ gbc-comp3095-springpet-MONDAY/src/main/java/ca/gbc/comp3095/demo/GbcComp3095SpringpetMondayApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbcComp3095SpringpetMondayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbcComp3095SpringpetMondayApplication.class, args);
+	}
+
+}
---
+++ gbc-comp3095-springpet-MONDAY/src/test/java/ca/gbc/comp3095/demo/GbcComp3095SpringpetMondayApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbcComp3095SpringpetMondayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
